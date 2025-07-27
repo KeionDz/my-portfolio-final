586498b52ebb0faaea782363648df814b72fb235
KeionDz <jkbd0135@gmail.com>
2025-07-27 14:43:58 +0800

    final logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-         <link rel="icon" href="../public/images/Kletter.png" />
+         <link rel="icon" href="/Kletter.svg" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
